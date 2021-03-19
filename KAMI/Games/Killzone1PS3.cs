41e5d019847357c7fcae33c39220845211253141
isJuhn <johanhellgren.98@gmail.com>
2021-03-19 18:32:46 +0100

    Implement DerefChain, not optimal for now but it works

--- KAMI/Games/Killzone1PS3.cs
+++ KAMI/Games/Killzone1PS3.cs
@@ -1,4 +1,5 @@
 ﻿using KAMI.Cameras;
+using KAMI.Utilities;
 using System;
 
 namespace KAMI.Games
@@ -7,33 +8,28 @@ namespace KAMI.Games
     {
         const uint BaseAddress = 0x828734;
 
-        uint m_addressHor;
-        uint m_addressVert;
+        DerefChain m_hor;
+        DerefChain m_vert;
 
         public Killzone1PS3(IntPtr ipc) : base(ipc)
         {
-        }
-
-        public override void InjectionStart()
-        {
-            uint p1 = IPCUtils.ReadU32(m_ipc, BaseAddress);
-            uint p2 = IPCUtils.ReadU32(m_ipc, p1 + 0x78);
-            uint p3 = IPCUtils.ReadU32(m_ipc, p2 + 0x220);
-            uint p4 = IPCUtils.ReadU32(m_ipc, p3 + 0xD8);
-            uint p5 = IPCUtils.ReadU32(m_ipc, p4 + 0x31C);
-            m_addressVert = p4 + 0x14c;
-            m_addressHor = p5 + 0x78;
+            var baseChain = DerefChain.CreateDerefChain(ipc, BaseAddress, 0x78, 0x220, 0xD8);
+            m_vert = baseChain.Chain(0x14c);
+            m_hor = baseChain.Chain(0x31C).Chain(0x78);
         }
 
         public override void UpdateCamera(int diffX, int diffY)
         {
-            m_camera.HorX = IPCUtils.ReadFloat(m_ipc, m_addressHor);
-            m_camera.HorY = IPCUtils.ReadFloat(m_ipc, m_addressHor + 4);
-            m_camera.Vert = IPCUtils.ReadFloat(m_ipc, m_addressVert);
-            m_camera.Update(-diffX * SensModifier, -diffY * SensModifier);
-            IPCUtils.WriteFloat(m_ipc, m_addressHor, m_camera.HorX);
-            IPCUtils.WriteFloat(m_ipc, m_addressHor + 4, m_camera.HorY);
-            IPCUtils.WriteFloat(m_ipc, m_addressVert, m_camera.Vert);
+            if (DerefChain.VerifyChains(m_hor, m_vert))
+            {
+                m_camera.HorY = IPCUtils.ReadFloat(m_ipc, (uint)m_hor.Value);
+                m_camera.HorX = IPCUtils.ReadFloat(m_ipc, (uint)(m_hor.Value + 4));
+                m_camera.Vert = IPCUtils.ReadFloat(m_ipc, (uint)m_vert.Value);
+                m_camera.Update(-diffX * SensModifier, -diffY * SensModifier);
+                IPCUtils.WriteFloat(m_ipc, (uint)m_hor.Value, m_camera.HorY);
+                IPCUtils.WriteFloat(m_ipc, (uint)(m_hor.Value + 4), m_camera.HorX);
+                IPCUtils.WriteFloat(m_ipc, (uint)m_vert.Value, m_camera.Vert);
+            }
         }
     }
 }
