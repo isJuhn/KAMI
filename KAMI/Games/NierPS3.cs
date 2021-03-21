775c7b858ee4534077e13f67898d49d8ed569d97
dio-gh <10439488+dio-gh@users.noreply.github.com>
2021-03-21 22:01:36 +0100

    add support for Nier
    Adds mouse injection support for Nier.

---
+++ KAMI/Games/NierPS3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using KAMI.Cameras;
+using KAMI.Utilities;
+
+namespace KAMI.Games
+{
+    class NierPS3 : Game<HAVACamera>
+    {
+        const uint BaseAddress = 0x1126854;
+
+        DerefChain m_hor;
+        DerefChain m_vert;
+
+        public NierPS3(IntPtr ipc) : base(ipc)
+        {
+            var baseChain = DerefChain.CreateDerefChain(ipc, BaseAddress, 0x7c);
+            m_hor = baseChain.Chain(0x1a8);
+            m_vert = baseChain.Chain(0x1c4);
+        }
+
+        public override void UpdateCamera(int diffX, int diffY)
+        {
+            if (DerefChain.VerifyChains(m_hor, m_vert))
+            {
+                m_camera.Hor = (float)(IPCUtils.ReadFloat(m_ipc, (uint)m_hor.Value));
+                m_camera.Vert = (float)(IPCUtils.ReadFloat(m_ipc, (uint)m_vert.Value));
+
+                m_camera.Update(-diffX * SensModifier, diffY * SensModifier);
+
+                IPCUtils.WriteFloat(m_ipc, (uint)m_hor.Value, m_camera.Hor);
+                IPCUtils.WriteFloat(m_ipc, (uint)m_vert.Value, m_camera.Vert);
+            }
+        }
+    }
+}
