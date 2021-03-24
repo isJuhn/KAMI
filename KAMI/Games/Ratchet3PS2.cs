b2681a4c80c7dccc37f1c667ba69d51884913456
isJuhn <johanhellgren.98@gmail.com>
2021-03-24 20:16:21 +0100

    Add support for Resistance 1

--- KAMI/Games/Ratchet3PS2.cs
+++ KAMI/Games/Ratchet3PS2.cs
@@ -6,10 +6,6 @@ namespace KAMI.Games
     {
         public Ratchet3PS2(IntPtr ipc) : base(ipc)
         {
-        }
-
-        public override void InjectionStart()
-        {
             m_addressHor = 0x1A6160;
             m_addressVert = 0x1A6180;
         }
---
+++ KAMI/Games/Resistance1.cs
@@ -0,0 +1,23 @@
+﻿using KAMI.Cameras;
+using System;
+
+namespace KAMI.Games
+{
+    public class Resistance1 : Game<HAVACamera>
+    {
+        const uint m_address = 0x4874C580;
+
+        public Resistance1(IntPtr ipc) : base(ipc)
+        {
+        }
+
+        public override void UpdateCamera(int diffX, int diffY)
+        {
+            m_camera.Vert = IPCUtils.ReadFloat(m_ipc, m_address);
+            m_camera.Hor = IPCUtils.ReadFloat(m_ipc, m_address + 4);
+            m_camera.Update(-diffX * SensModifier, diffY * SensModifier);
+            IPCUtils.WriteFloat(m_ipc, m_address, m_camera.Vert);
+            IPCUtils.WriteFloat(m_ipc, m_address + 4, m_camera.Hor);
+        }
+    }
+}
