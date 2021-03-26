9b6842b9188446db9cde5cf01ebc1851b1fcec05
isJuhn <johanhellgren.98@gmail.com>
2021-03-26 23:09:50 +0100

    Add support for Killzone 3

---
+++ KAMI/Games/Killzone3PS3.cs
@@ -0,0 +1,35 @@
+﻿using KAMI.Cameras;
+using KAMI.Utilities;
+using System;
+
+namespace KAMI.Games
+{
+    public class Killzone3PS3 : Game<HVecVACamera>
+    {
+        const uint BaseAddress = 0x1587880 + 0x760;
+
+        DerefChain m_hor;
+        DerefChain m_vert;
+
+        public Killzone3PS3(IntPtr ipc) : base(ipc)
+        {
+            var baseChain = DerefChain.CreateDerefChain(ipc, BaseAddress, 0x38, 0x0, 0x68);
+            m_vert = baseChain.Chain(0x150).Chain(0xA0);
+            m_hor = baseChain.Chain(0xFC).Chain(0x14).Chain(0xB0);
+        }
+
+        public override void UpdateCamera(int diffX, int diffY)
+        {
+            if (DerefChain.VerifyChains(m_hor, m_vert))
+            {
+                m_camera.HorY = IPCUtils.ReadFloat(m_ipc, (uint)m_hor.Value);
+                m_camera.HorX = IPCUtils.ReadFloat(m_ipc, (uint)(m_hor.Value + 4));
+                m_camera.Vert = IPCUtils.ReadFloat(m_ipc, (uint)m_vert.Value);
+                m_camera.Update(diffX * SensModifier, -diffY * SensModifier);
+                IPCUtils.WriteFloat(m_ipc, (uint)m_hor.Value, m_camera.HorY);
+                IPCUtils.WriteFloat(m_ipc, (uint)(m_hor.Value + 4), m_camera.HorX);
+                IPCUtils.WriteFloat(m_ipc, (uint)m_vert.Value, m_camera.Vert);
+            }
+        }
+    }
+}
