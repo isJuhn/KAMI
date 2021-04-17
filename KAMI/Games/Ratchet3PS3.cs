30a4f98c5314ec138c0fc6557cf738a5b74058b5
isJuhn <johanhellgren.98@gmail.com>
2021-04-17 16:13:51 +0200

    Add support for ratchet deadlocked and some mp stuff

--- KAMI/Games/Ratchet3PS3.cs
+++ KAMI/Games/Ratchet3PS3.cs
@@ -8,8 +8,21 @@ namespace KAMI.Games
     {
         public Ratchet3PS3(IntPtr ipc) : base(ipc)
         {
-            m_addressHor = 0xDA3D70;
-            m_addressVert = 0xDA3D90;
+        }
+
+        public override void UpdateCamera(int diffX, int diffY)
+        {
+            if (IPCUtils.ReadU32(m_ipc, 0x13DD584) > 0) // MP player 1 initialized or something
+            {
+                m_addressHor = 0x13DE760;
+                m_addressVert = 0x13DE780;
+            }
+            else
+            {
+                m_addressHor = 0xDA3D70;
+                m_addressVert = 0xDA3D90;
+            }
+            base.UpdateCamera(diffX, diffY);
         }
     }
 }
