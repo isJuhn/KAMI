a183a7862147e0ed817de279a317c494c4d23ed7
isJuhn <johanhellgren.98@gmail.com>
2021-04-17 16:15:39 +0200

    Implement mouse hook and cursor hiding

---
+++ KAMI/MouseCursor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KAMI
+{
+    public static class MouseCursor
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr CreateCursor(IntPtr hInst, int xHotSpot, int yHotSpot, int nWidth, int nHeight, byte[] pvANDPlane, byte[] pvXORPlane);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetSystemCursor(IntPtr hcur, uint id);
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+
+        const uint OCR_NORMAL = 32512;
+        const uint SPI_SETCURSORS = 0x0057;
+
+
+        public static bool HideCursor()
+        {
+            Module[] list = Assembly.GetExecutingAssembly().GetModules();
+            IntPtr hInst = Marshal.GetHINSTANCE(list[0]);
+            byte[] pvANDPlane = { 0xff };
+            byte[] pvXORPlane = { 0x00 };
+            IntPtr cursor = CreateCursor(hInst, 0, 0, 1, 1, pvANDPlane, pvXORPlane);
+            return SetSystemCursor(cursor, OCR_NORMAL);
+        }
+
+        public static bool ShowCursor()
+        {
+            return SystemParametersInfo(SPI_SETCURSORS, 0, IntPtr.Zero, 0);
+        }
+    }
+}
