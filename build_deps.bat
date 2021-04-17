faf3126c9958ea3d1b15298289087104a0954494
isJuhn <johanhellgren.98@gmail.com>
2021-04-17 16:11:48 +0200

    Add inputsimulator submodule

--- build_deps.bat
+++ build_deps.bat
@@ -4,3 +4,6 @@ cd "build"
 ninja
 cd "../../../../"
 xcopy "pcsx2_ipc\bindings\c\build\pcsx2_ipc_c.dll" "KAMI" /Y
+
+devenv "inputsimulator/WindowsInput/WindowsInput.csproj" /Build "Release"
+xcopy "inputsimulator\WindowsInput\bin\Release\WindowsInput.dll" "KAMI" /Y
