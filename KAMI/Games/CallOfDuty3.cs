6a24adad5c0a6d9c06ed4fce43941272e82e1aac
isJuhn <johanhellgren.98@gmail.com>
2021-05-22 19:17:57 +0200

    Initial cod3 support

---
+++ KAMI/Games/CallOfDuty3.cs
@@ -0,0 +1,23 @@
+﻿using KAMI.Cameras;
+using System;
+
+namespace KAMI.Games
+{
+    public class CallOfDuty3 : Game<HAVACamera>
+    {
+        const uint m_addr = 0x101A17C4;
+
+        public CallOfDuty3(IntPtr ipc) : base(ipc)
+        {
+        }
+
+        public override void UpdateCamera(int diffX, int diffY)
+        {
+            m_camera.Vert = (float)(IPCUtils.ReadFloat(m_ipc, m_addr) * (Math.PI / 180));
+            m_camera.Hor = (float)(IPCUtils.ReadFloat(m_ipc, m_addr + 4) * (Math.PI / 180));
+            m_camera.Update(-diffX * SensModifier, diffY * SensModifier);
+            IPCUtils.WriteFloat(m_ipc, m_addr, (float)(m_camera.Vert * (180 / Math.PI)));
+            IPCUtils.WriteFloat(m_ipc, m_addr + 4, (float)(m_camera.Hor * (180 / Math.PI)));
+        }
+    }
+}
