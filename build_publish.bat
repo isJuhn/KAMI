411f2b45b599b87c6e8b25ef2c1705c87d90aae5
Margen67 <Margen67@users.noreply.github.com>
2022-01-25 18:56:06 -0800

    GitHub Actions CI

--- build_publish.bat
+++ build_publish.bat
@@ -1,1 +1,1 @@
-dotnet publish "KAMI.Windows/KAMI.Windows.csproj" -c Release -o "bin/publish/"
+dotnet publish /p:DebugType=None /p:DebugSymbols=false "KAMI.Windows/KAMI.Windows.csproj" -c Release -o "bin/publish/"
