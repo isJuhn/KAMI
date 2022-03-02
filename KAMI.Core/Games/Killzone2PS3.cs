9cfcafc515a9a72af0f6e0c77bad3481806a6dcc
isJuhn <johanhellgren.98@gmail.com>
2022-03-02 20:02:56 +0100

    Add support for killzone 2 version 01.29

--- KAMI.Core/Games/Killzone2PS3.cs
+++ KAMI.Core/Games/Killzone2PS3.cs
@@ -6,14 +6,18 @@ namespace KAMI.Core.Games
 {
     public class Killzone2PS3 : Game<HVecVACamera>
     {
-        const uint BaseAddress = 0x117e740 + 0x234;
-
         DerefChain m_hor;
         DerefChain m_vert;
 
-        public Killzone2PS3(IntPtr ipc) : base(ipc)
+        public Killzone2PS3(IntPtr ipc, string version) : base(ipc)
         {
-            var baseChain = DerefChain.CreateDerefChain(ipc, BaseAddress, 0x0);
+            uint baseAddress = version switch
+            {
+                "01.01" => 0x117e740 + 0x234,
+                "01.29" => 0x11B0540 + 0x234,
+                _ => throw new NotImplementedException($"{nameof(Killzone2PS3)} [v'{version}'] is not implemented"),
+            };
+            var baseChain = DerefChain.CreateDerefChain(ipc, baseAddress, 0x0);
             m_vert = baseChain.Chain(0x80).Chain(0x5c).Chain(0x11c).Chain(0x78);
             m_hor = baseChain.Chain(0x78).Chain(0x0).Chain(0x68).Chain(0xc).Chain(0x90);
         }
