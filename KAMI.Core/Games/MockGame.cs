42a1ea8a99185546021fc96aab2dc8227363673b
isJuhn <johanhellgren.98@gmail.com>
2023-04-08 14:03:44 +0200

    Handle PCSX2 BIOS 'serials'

---
+++ KAMI.Core/Games/MockGame.cs
@@ -0,0 +1,15 @@
+﻿namespace KAMI.Core.Games
+{
+    internal class MockGame : IGame
+    {
+        public float SensModifier { get; set; }
+
+        public void InjectionStart()
+        {
+        }
+
+        public void UpdateCamera(int diffX, int diffY)
+        {
+        }
+    }
+}
